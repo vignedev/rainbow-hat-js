b364c2bc677724db47d5f10636e6aca81915ad6f
vignedev <vignedev@gmail.com>
2018-03-15 13:56:00 +0100

    correct name of a component

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
 	display: new (require('./lib/alphanum4.js'))(),
-	lights: require('./lib/apa102.js'),
+	rainbow: require('./lib/apa102.js'),
 	touch: require('./lib/touch.js')
 }
